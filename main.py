7e589cb8c775cfae2c2eab3e9d94ab88f074d8d0
youngkiu <youngkiu@gmail.com>
2021-01-07 22:11:26 +0900

    multiple extensions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import time
 import csv
 import json
+import pandas as pd
 
 def get_num_of_files_with_ext(token, ext, repo):
     # https://stackoverflow.com/a/48192349
@@ -72,11 +73,18 @@ if __name__ == '__main__':
     _token = sys.argv[1]
 
     _org_name = 'vuno'
-    _file_ext = 'sh'
-    _repo_ext_path_dict = {}
+    _file_exts = ['sh', 'yml', 'py', 'js']
+
+    _columns = ['repo name', ]
+    for _file_ext in _file_exts:
+        _columns.append(f'.{_file_ext} files')
+
+    # https://stackoverflow.com/a/24888331/6572046
+    df = pd.DataFrame(columns=_columns)
 
     # https://stackoverflow.com/questions/30656761/github-search-api-only-return-30-results
     _page_num = 1  # github default page_size: 30
+    _row_index = 0
     while True:
         _repo_list = get_repositories(_token, _page_num)
         if not _repo_list:
@@ -86,22 +94,47 @@ if __name__ == '__main__':
             if not _repo.startswith(_org_name):
                 continue
 
-            _path_list = get_num_of_files_with_ext(_token, _file_ext, _repo)
-            time.sleep(2)  # Don't delete this code. Without this code, will get a 403 return
-            if isinstance(_path_list, list):
-                _name = _repo[_repo.find('/')+1:]
-                _repo_ext_path_dict[_name] = _path_list
-                print(_name, len(_path_list), _path_list)
-            else:
-                print(f'[Error] {_org_name}: {_path_list}')
+            _name = _repo[_repo.find('/')+1:]
+            _row = [_name, ]
+
+            for _file_ext in _file_exts:
+                _path_list = get_num_of_files_with_ext(_token, _file_ext, _repo)
+                if isinstance(_path_list, list):
+                    print(_name, f'.{_file_ext}', len(_path_list), _path_list)
+                else:
+                    print(f'[Error] {_org_name}: {_path_list}')
+                _row.append(_path_list)
+                time.sleep(2)  # Don't delete this code. Without this code, will get a 403 return
+
+            df.loc[_row_index] = _row
+            _row_index += 1
 
         _page_num += 1
 
-    # print(json.dumps(_repo_ext_path_dict, indent=4))
-
-    csv_path = f'{_org_name}_repo.{_file_ext}_file_num.csv'
+    csv_path = f'{_org_name}_repo_file_num.csv'
     with open(csv_path, 'w', encoding='UTF-8') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(['repo name', f'file paths with .{_file_ext} ext', 'total count'])
-        for repo_name, file_paths in _repo_ext_path_dict.items():
-            csv_writer.writerow([repo_name, '\n'.join(file_paths), len(file_paths)])
+
+        csv_row = []
+        for i, col in enumerate(_columns):
+            csv_row.append(col)
+            if i == 0:
+                continue
+            csv_row.append(f'# {col}')
+
+        print(csv_row)
+        csv_writer.writerow(csv_row)
+
+        # https://stackoverflow.com/a/16476974/6572046
+        for index, row in df.iterrows():
+            csv_row = []
+
+            for i in range(len(_columns)):
+                cell_data = row[_columns[i]]
+                if i == 0:
+                    csv_row.append(cell_data)
+                else:
+                    csv_row.append('\n'.join(cell_data))
+                    csv_row.append(len(cell_data))
+
+            csv_writer.writerow(csv_row)
